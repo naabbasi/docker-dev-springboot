3f6c02bca0ebf9e149ac9f3a304a978bb0ccd6f8
Noman Ali Abbasi <noman.ali.abbasy@gmail.com>
2022-01-22 17:12:18 +0300

    Initial commit

---
+++ src/main/java/edu/learn/DockerDevApplication.java
@@ -0,0 +1,11 @@
+package edu.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDevApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDevApplication.class);
+    }
+}
---
+++ src/test/java/edu/learn/DockerDevApplicationTest.java
@@ -0,0 +1,12 @@
+package edu.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DockerDevApplicationTest {
+
+    @Test
+    public void test1(){
+    }
+}
